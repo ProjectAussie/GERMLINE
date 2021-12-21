e1534ddc08bef0263ab121cf4e08e63ef76e18a8
Mike Zhong <mzhong@embarkvet.com>
2021-12-21 12:03:36 -0800

    Deleted log files, updated runner, test results are from binary built on ubuntu 20.04

--- minimal_test/run_new.sh
+++ minimal_test/run_new.sh
@@ -4,7 +4,7 @@ set -e
 
 
 if [ "$1" == "old-new" ]; then
-    ~/dev/GERMLINE/germline -haploid -min_m 0.5 -err_hom 0 -err_het 0 -bits 90 -w_extend \
+    ../germline -haploid -min_m 0.5 -err_hom 0 -err_het 0 -bits 90 -w_extend \
         -samples_to_compare_to 0000_old_dog_proxy_keys_germline_output.plinky \
         -new_samples 0000_new_dog_proxy_keys_germline_output.plinky \
         -chromosome 1 \
@@ -13,7 +13,7 @@ if [ "$1" == "old-new" ]; then
 fi
 
 if [ "$1" == "new-new" ]; then
-    ~/dev/GERMLINE/germline -haploid -min_m 0.5 -err_hom 0 -err_het 0 -bits 90 -w_extend \
+    ../germline -haploid -min_m 0.5 -err_hom 0 -err_het 0 -bits 90 -w_extend \
         -samples_to_compare_to 0000_new_dog_proxy_keys_germline_output.plinky \
         -new_samples 0000_new_dog_proxy_keys_germline_output.plinky \
         -chromosome 1 \
