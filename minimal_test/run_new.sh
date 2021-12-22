0566a1ad7c50e16fd2b4a6c1622f81dbd43cfa12
Mike Zhong <mzhong@embarkvet.com>
2021-12-22 13:15:13 -0800

    Fixed

--- minimal_test/run_new.sh
+++ minimal_test/run_new.sh
@@ -7,9 +7,9 @@ if [ "$1" == "old-new" ]; then
     ../germline -haploid -min_m 0.5 -err_hom 0 -err_het 0 -bits 90 -w_extend \
         -samples_to_compare_to 0000_old_dog_proxy_keys_germline_output.plinky \
         -new_samples 0000_new_dog_proxy_keys_germline_output.plinky \
-        -chromosome 1 \
-        -individual_outputs test_a \
         < germline_params_old_new
+ #       -chromosome 1 \
+#        -individual_outputs test_a \
 fi
 
 if [ "$1" == "new-new" ]; then
